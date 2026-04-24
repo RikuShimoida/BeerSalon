0631617e3f37811da483231a8b71afeb4803d98c
Riku Shimoida <xiajingtianlu@gmail.com>
2026-04-25 00:03:51 +0900

    add: hooksの追加

---
+++ apps/web/.next/server/middleware-build-manifest 2.js
@@ -0,0 +1,22 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/3e427c9146f6df15.js",
+    "static/chunks/3f3758e8e76c5fc6.js",
+    "static/chunks/045ccb0a3fffc30e.js",
+    "static/chunks/318d71108d464296.js",
+    "static/chunks/b7d8a7bbd834c398.js",
+    "static/chunks/turbopack-8ad39d0889f4fadc.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
